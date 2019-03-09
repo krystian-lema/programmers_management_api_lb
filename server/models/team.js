3d40ffe952e1060e76701963c3a09dfbd44bc220
Krystian Lema <krystian.lema@gmail.com>
2019-03-09 16:10:54 +0100

    Add include all data method to teams.

--- server/models/team.js
+++ server/models/team.js
@@ -2,4 +2,35 @@
 
 module.exports = function(Team) {
 
+    /**
+    * Include all related models.
+    * @param {Function(Error, object)} callback
+    */
+
+    Team.include_all_data = function(callback) {
+        var result = [];
+        var filter = {
+            // fields: ['id', 'title', 'content', 'author'],
+            include: {programmers: {languages: ['paradigms']}}
+        };
+
+        Team.find(filter, function(err, teams) {
+            if (err !== null) {
+                return callback(err);
+            }
+
+            if (teams.length) {
+                teams.forEach(function(team) {
+                    result.push(team);
+                });
+            }
+
+            if (result.length) {
+                callback(null, result);
+            }
+            else {
+                callback(null, []);
+            }
+        });
+    };
 };
